c0eaf8451725f86cbe05b73412eefdb32fb6bb11
GrodrZ <fernandogroder@gmail.com>
2022-07-04 21:06:29 -0300

    feat: add telegram

--- src/scripts/telegram.js
+++ src/scripts/telegram.js
@@ -8,20 +8,6 @@ const TELEGRAM_BOT_CHAT_ID = process.env.TELEGRAM_BOT_CHAT_ID;
 const postNews = async (latestNewsInfo) => {
     try {
         console.log(Date() + '- Sendind message in Telegram...');
-        // axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
-        //     chat_id: TELEGRAM_BOT_CHAT_ID,
-        //     text: `${latestNewsInfo.title}`,
-        //     parse_mode: 'Markdown',
-        //     disable_web_page_preview: true,
-        //     reply_markup: {
-        //         inline_keyboard: [
-        //             [{ text: 'Leia mais', url: latestNewsInfo.link }]
-        //         ]
-        //     }
-        // }).then(response => {
-        //     console.log(Date() + '- Telegram message sent');
-        // }).catch(console.error);
-
         axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendPhoto`, {
             chat_id: TELEGRAM_BOT_CHAT_ID,
             photo: latestNewsInfo.imageUrl,
