a746e21e800250a18574beab48718942dfc1dca5
GrodrZ <fernandogroder@gmail.com>
2022-07-04 21:04:53 -0300

    feat: add telegram

--- src/scripts/twitter.js
+++ src/scripts/twitter.js
@@ -1,19 +1,18 @@
-// const axios = require('axios');
 const { TwitterApi, EUploadMimeType } = require('twitter-api-v2');
 const axios = require('axios');
 const fs = require('fs');
 
 require('dotenv').config()
-const API_KEY = process.env.API_KEY;
-const API_KEY_SECRET = process.env.API_KEY_SECRET;
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
-const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+const TWITTER_API_KEY = process.env.TWITTER_API_KEY;
+const TWITTER_API_KEY_SECRET = process.env.TWITTER_API_KEY_SECRET;
+const TWITTER_ACCESS_TOKEN = process.env.TWITTER_ACCESS_TOKEN;
+const TWITTER_ACCESS_TOKEN_SECRET = process.env.TWITTER_ACCESS_TOKEN_SECRET;
 
 const client = new TwitterApi({
-    accessToken: ACCESS_TOKEN,
-    accessSecret: ACCESS_TOKEN_SECRET,
-    appKey: API_KEY,
-    appSecret: API_KEY_SECRET,
+    accessToken: TWITTER_ACCESS_TOKEN,
+    accessSecret: TWITTER_ACCESS_TOKEN_SECRET,
+    appKey: TWITTER_API_KEY,
+    appSecret: TWITTER_API_KEY_SECRET,
 });
 
 const rwClient = client.readWrite;
@@ -21,7 +20,7 @@ const rwClient = client.readWrite;
 const tweetNews = async (latestNewsInfo) => {
     try {
         console.log(Date() + '- Uploading image...');  
-        await uploadImage(latestNewsInfo.imageUrl).then(response => {
+        client.v1.uploadMedia('./public/images/image.jpg', { mimeType: EUploadMimeType.Png }).then(response => {
             console.log(Date() + '- Image Uploaded'); 
             console.log(Date() + '- Tweeting...');  
             rwClient.v1.tweet(`${latestNewsInfo.title}`, { media_ids: response}).then(response => {
@@ -37,37 +36,6 @@ const tweetNews = async (latestNewsInfo) => {
     } catch (error) {
         console.log(error);
     }
-}
-
-function uploadImage(imageUrl) {
-    return new Promise(resolve =>{
-        try {
-            console.log(Date() + '- Downloading image...');    
-            axios({
-                method: "GET",
-                url: imageUrl,
-                responseType: "stream",   
-            }).then(response => {
-                const image = fs.createWriteStream('./public/images/image.jpg');
-                response.data.pipe(image);
-                image.on('finish', () => {
-                    image.close();
-                    resolve(client.v1.uploadMedia('./public/images/image.jpg', { mimeType: EUploadMimeType.Png }));
-                    //delete image
-                    fs.unlink('./public/images/image.jpg', (err) => {
-                        if (err) throw err;
-                        console.log(Date() + '- Image Deleted'); 
-                    }
-                    );
-                }
-                );
-            }
-            ).catch(console.error);
-        } catch (err) {
-        console.log(err);
-        }
-    }
-    );
 }
 
 module.exports = { tweetNews };
